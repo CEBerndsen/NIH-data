22331ceace0af548d9cc1b3800e2f43993ff30d8
CEBerndsen <berndsce@jmu.edu>
2018-07-26 12:27:08 -0400

    initial commit

---
+++ Data/processing.R
@@ -0,0 +1,31 @@
+# load packages
+library(tidyverse)
+library(readxl)
+library(tools)
+
+# get file list and names
+files <- dir(pattern = "*.xlsx")
+
+# make function to trim and combine data sets into single dataframe
+
+NIH_state_year <- function(x){
+  #load dataset
+  df <- read_excel(x)
+  
+  year <- file_path_sans_ext(x)
+  
+  #trim data and remove non-US countries
+  df_tot <- df %>%
+    filter(!is.na(STATE)) %>%
+    filter(COUNTRY == "UNITED STATES") %>%
+    group_by(STATE) %>%
+    # summarize the dollars and number of awards by state
+    summarise(fund_tot = sum(FUNDING), numaward_tot = sum(AWARDS)) %>%
+    #add a year column
+    mutate(yr = year)
+  
+}
+
+# Use map_dfr to run over all the datasets and combine them into a single dataframe
+NIH_annual <- map_dfr(files, NIH_state_year)
+
